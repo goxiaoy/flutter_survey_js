c603a0f13b7d7d22000cfa76a7ea6b82bbb46bd3
laogao <gaoyuxiang@gmail.com>
2023-02-20 12:07:17 +0800

    ups deps, now runnable on flutter 3.7.x

--- lib/ui/reactive/reactive_color_picker.dart
+++ lib/ui/reactive/reactive_color_picker.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
-import 'package:flutter_survey_js/utils.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
 class ReactiveColorPicker extends ReactiveFormField<String, Color> {
