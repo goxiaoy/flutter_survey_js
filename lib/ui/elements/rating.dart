6ccef64478e249068a7b438ec294fac2d0e75a4b
David Chopin <david.chopin@wwt.com>
2023-03-20 11:15:09 -0500

    Made it so that selected Rating values get a white text color - DC

--- lib/ui/elements/rating.dart
+++ lib/ui/elements/rating.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_survey_js/survey.dart' as s;
+import 'package:reactive_forms/reactive_forms.dart';
 import 'package:reactive_segmented_control/reactive_segmented_control.dart';
 
 import 'question_title.dart';
@@ -9,25 +9,54 @@ import 'survey_element_factory.dart';
 final SurveyElementBuilder ratingBuilder =
     (context, element, {bool hasTitle = true}) {
   final e = element as s.Rating;
+  FormGroup buildForm() =>
+      fb.group({element.name!: FormControl<int>(value: e.rateMin ?? 1)});
 
-  final children = <Object, Widget>{};
-  if (e.rateValues != null && e.rateValues!.length > 0) {
-    for (final v in e.rateValues!) {
-      children[v.value] = Text(v.text ?? v.value?.toString() ?? '');
+  final textStyle = Theme.of(context)
+      .textTheme
+      .bodyText2
+      ?.copyWith(color: Theme.of(context).primaryColor);
+
+  Map<int, Widget> getChildren({required int selectedValue}) {
+    final children = <int, Widget>{};
+    if (e.rateValues != null && e.rateValues!.length > 0) {
+      for (final v in e.rateValues!) {
+        children[v.value] = Text(
+          v.text ?? v.value?.toString() ?? '',
+          style: textStyle,
+        );
+      }
+    } else {
+      //use max, min,step
+      final maxValue = e.rateMax ?? 5;
+      final min = e.rateMin ?? 1;
+      final step = e.rateStep ?? 1;
+      var current = min;
+      while (current <= maxValue) {
+        children[current] = Text(
+          current.toString(),
+          style: selectedValue == current
+              ? textStyle?.copyWith(color: Colors.white)
+              : textStyle,
+        );
+        current += step;
+      }
     }
-  } else {
-    //use max, min,step
-    final maxValue = e.rateMax ?? 5;
-    final min = e.rateMin ?? 1;
-    final step = e.rateStep ?? 1;
-    var current = min;
-    while (current <= maxValue) {
-      children[current] = Text(current.toString());
-      current += step;
-    }
+    return children;
   }
-  return ReactiveSegmentedControl(
-    formControlName: element.name!,
-    children: children,
-  ).wrapQuestionTitle(element, hasTitle: hasTitle);
+
+  return ReactiveFormBuilder(
+    form: buildForm,
+    builder: (context, formGroup, child) {
+      return ReactiveValueListenableBuilder(
+        formControl: formGroup.control(element.name!),
+        builder: (context, control, child) {
+          return ReactiveSegmentedControl<int, int>(
+            formControlName: element.name!,
+            children: getChildren(selectedValue: control.value as int),
+          ).wrapQuestionTitle(element, hasTitle: hasTitle);
+        },
+      );
+    },
+  );
 };
