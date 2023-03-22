5d72f3f0ce2127bd535cbc1ecdf3250fe3faa8a9
David Chopin <david.chopin@wwt.com>
2023-03-22 11:06:15 -0500

    Added test verifying that the SurveyController passed into SurveyWidget attaches to the SurveyProvider - DC

--- test/ui/survey_widget_test.dart
+++ test/ui/survey_widget_test.dart
@@ -1,7 +1,6 @@
 import 'dart:core';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_survey_js/survey.dart';
 import 'package:flutter_survey_js/survey.dart' as s;
 import 'package:flutter_test/flutter_test.dart';
 
@@ -11,7 +10,7 @@ main() {
   group('SurveyController', () {
     group('submit', () {
       testWidgets('calls SurveyWidget.onSubmit', (widgetTester) async {
-        final SurveyController controller = SurveyController();
+        final s.SurveyController controller = s.SurveyController();
         int onSubmitCallCount = 0;
         await widgetTester.pumpWidget(
           MaterialApp(
@@ -19,7 +18,7 @@ main() {
               s.MultiAppLocalizationsDelegate(),
             ],
             home: Material(
-              child: SurveyWidget(
+              child: s.SurveyWidget(
                 survey: TestData.survey(
                   pages: [
                     TestData.page(
@@ -40,6 +39,42 @@ main() {
         controller.submit();
         expect(onSubmitCallCount, 1);
       });
+    });
+
+    testWidgets('attaches to SurveyProvider', (widgetTester) async {
+      final s.SurveyController controller = s.SurveyController();
+      final String submitButtonText = 'Click me to submit!';
+      int onSubmitCallCount = 0;
+      await widgetTester.pumpWidget(
+        MaterialApp(
+          localizationsDelegates: [
+            s.MultiAppLocalizationsDelegate(),
+          ],
+          home: Material(
+            child: s.SurveyWidget(
+              survey: TestData.survey(
+                pages: [
+                  TestData.page(
+                    elements: [
+                      s.Text(),
+                    ],
+                  ),
+                ],
+              ),
+              controller: controller,
+              builder: (context) => TextButton(
+                onPressed: s.SurveyProvider.of(context).controller?.submit,
+                child: Text(submitButtonText),
+              ),
+              onSubmit: (_) => onSubmitCallCount++,
+            ),
+          ),
+        ),
+      );
+      await widgetTester.pump();
+      await widgetTester.idle();
+      await widgetTester.tap(find.text(submitButtonText));
+      expect(onSubmitCallCount, 1);
     });
   });
 }
