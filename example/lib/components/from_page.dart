3445591399175642d3f5f1c8e2edc577b6580a0b
Tyler Jones <tyler.jones2@wwt.com>
2023-04-10 08:49:14 -0500

    Added a fromPage SurveyWidget factory method to support surveys that only have page data when pagination is disabled

---
+++ example/lib/components/from_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_survey_js/survey.dart' as s;
+
+class FromPage extends StatelessWidget {
+  final s.Page? page;
+
+  const FromPage({Key? key, this.page}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Survey test'),
+            ),
+            body: page == null
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : s.SurveyWidget.fromPage(
+                    page: page!,
+                    onChange: (v) {
+                      print(v);
+                    },
+                    onSubmit: (v) {
+                      print(v);
+                      showModalBottomSheet<void>(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return Container(
+                            height: 400,
+                            child: Center(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Expanded(
+                                      child: Container(
+                                          child: SingleChildScrollView(
+                                              child: Text(v.toString())))),
+                                  ElevatedButton(
+                                    child: const Text('Close'),
+                                    onPressed: () => Navigator.pop(context),
+                                  )
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  )));
+  }
+}
