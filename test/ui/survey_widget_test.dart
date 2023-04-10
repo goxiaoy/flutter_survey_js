5fd64b80c07ebda375e4a3a5754c793b5096db4f
David Chopin <david.chopin@wwt.com>
2023-04-10 09:45:41 -0500

    Added SurveyWidget fromPage factory test - DC/TJ

--- test/ui/survey_widget_test.dart
+++ test/ui/survey_widget_test.dart
@@ -42,4 +42,31 @@ main() {
       });
     });
   });
+
+  group('fromPage', () {
+    testWidgets('displays title and elements of page', (widgetTester) async {
+      final String pageTitle = 'Page 1';
+      final String questionName = 'Question 1';
+      await widgetTester.pumpWidget(
+        MaterialApp(
+          localizationsDelegates: [
+            s.MultiAppLocalizationsDelegate(),
+          ],
+          home: Material(
+            child: SurveyWidget.fromPage(
+              page: TestData.page(
+                title: pageTitle,
+                elements: [s.Text()..name = questionName],
+              ),
+            ),
+          ),
+        ),
+      );
+      await widgetTester.pump();
+      await widgetTester.idle();
+
+      expect(find.text(pageTitle), findsOneWidget);
+      expect(find.text(questionName), findsOneWidget);
+    });
+  });
 }
