5a75b5e7c20034d3c8e127e31512b49f0dd464f4
Goxiaoy <xy1996mail@gmail.com>
2023-04-12 11:01:11 +0800

    fix lint

--- test/questions/text_test.dart
+++ test/questions/text_test.dart
@@ -101,7 +101,7 @@ void main() {
   });
 
   testWidgets('displays placeholder', (WidgetTester tester) async {
-    final placeholder = 'Enter some text here...';
+    const placeholder = 'Enter some text here...';
     final s = Survey.fromJson(
       {
         "pages": [
@@ -119,7 +119,7 @@ void main() {
     );
     await tester.pumpWidget(
       MaterialApp(
-        localizationsDelegates: [
+        localizationsDelegates: const [
           MultiAppLocalizationsDelegate(),
         ],
         home: Material(
