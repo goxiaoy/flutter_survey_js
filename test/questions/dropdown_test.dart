ab7373122d46ad3c1ca2b6851f9adbd2d90ac90d
David Chopin <david.chopin@wwt.com>
2023-04-18 15:08:13 -0500

    Wrote tests - DC/TJ

--- test/questions/dropdown_test.dart
+++ test/questions/dropdown_test.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_survey_js/generated/l10n.dart';
 import 'package:flutter_survey_js/model/survey.dart';
-import 'package:flutter_survey_js/multi_localization_delegate.dart';
 import 'package:flutter_survey_js/ui/survey_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 
 void main() {
+  const appLocalizationDelegate = AppLocalizationDelegate();
   // 单一的测试
   const json = {
     "questions": [
@@ -59,9 +60,7 @@ void main() {
       );
       await tester.pumpWidget(
         MaterialApp(
-          localizationsDelegates: const [
-            MultiAppLocalizationsDelegate(),
-          ],
+          localizationsDelegates: const [appLocalizationDelegate],
           home: Material(
             child: SurveyWidget(survey: s),
           ),
@@ -73,8 +72,6 @@ void main() {
       expect(find.text(placeholder), findsOneWidget);
     });
 
-    const AppLocalizationDelegate appLocalizationDelegate =
-        AppLocalizationDelegate();
     final locales = appLocalizationDelegate.supportedLocales;
 
     for (var locale in locales) {
@@ -128,5 +125,93 @@ void main() {
         expect(find.text(placeholderText), findsOneWidget);
       });
     }
+  });
+
+  testWidgets(
+      'maps otherText to DropdownMenuItem and displays ReactiveTextField when tapped',
+      (WidgetTester tester) async {
+    const otherText = "Special Request";
+
+    await tester.pumpWidget(
+      MaterialApp(
+        localizationsDelegates: const [
+          appLocalizationDelegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        home: Material(
+          child: SurveyWidget(
+              survey: Survey.fromJson({
+            "questions": [
+              {
+                "type": "dropdown",
+                "name": "What t-shirt size do you want?",
+                "isRequired": true,
+                "choices": [
+                  "S",
+                  "M",
+                  "L",
+                  "XL",
+                ],
+                "showOtherItem": true,
+                "otherText": otherText,
+              }
+            ]
+          })),
+        ),
+      ),
+    );
+
+    await tester.tap(find.byType(ReactiveDropdownField));
+    await tester.pump();
+    await tester.idle();
+
+    await tester.tap(find.text(otherText).last);
+    await tester.pump();
+    await tester.idle();
+
+    expect(find.byType(ReactiveTextField), findsOneWidget);
+  });
+
+  testWidgets('maps noneText to DropdownMenuItem', (WidgetTester tester) async {
+    const noneText = "I do not want a t-shirt";
+
+    await tester.pumpWidget(
+      MaterialApp(
+        localizationsDelegates: const [
+          appLocalizationDelegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        home: Material(
+          child: SurveyWidget(
+              survey: Survey.fromJson({
+            "questions": [
+              {
+                "type": "dropdown",
+                "name": "What t-shirt size do you want?",
+                "isRequired": true,
+                "choices": [
+                  "S",
+                  "M",
+                  "L",
+                  "XL",
+                ],
+                "showNoneItem": true,
+                "noneText": noneText,
+              }
+            ]
+          })),
+        ),
+      ),
+    );
+
+    await tester.tap(find.byType(ReactiveDropdownField));
+    await tester.pump();
+    await tester.idle();
+
+    expect(find.text(noneText), findsWidgets);
   });
 }
