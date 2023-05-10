a91c365402a63b1b3261ec7ae55c1c0d2bfd2ab3
laogao <gaoyuxiang@gmail.com>
2023-05-10 11:36:31 +0800

    unit test for checkbox

--- test/questions/check_boxes_test.dart
+++ test/questions/check_boxes_test.dart
@@ -1,20 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_survey_js/generated/l10n.dart';
 import 'package:flutter_survey_js/survey.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  const appLocalizationDelegate = AppLocalizationDelegate();
   // 单一的测试
   const json = {
     "questions": [
       {
         "type": "checkbox",
         "name": "car",
-        "title": "What car are you driving?",
+        "title": "What cars are you driving?",
         "isRequired": true,
         "colCount": 4,
-        "hasOther": true,
-        "hasNone": true,
+        "showSelectAllItem": true,
+        "showNoneItem": true,
+        "showOtherItem": true,
         "choices": [
-          "None",
           "Ford",
           "Vauxhall",
           "Volkswagen",
@@ -31,5 +35,129 @@ void main() {
   };
   test("Serialize Deserialize Survey", () {
     final s = surveyFromJson(json);
+  });
+
+  const extended = {
+    "questions": [
+      {
+        "type": "checkbox",
+        "name": "warships",
+        "title": "Which battleships have you ever boarded?",
+        "isRequired": true,
+        "showSelectAllItem": true,
+        "showNoneItem": true,
+        "showOtherItem": true,
+        "selectAllText": "All of them",
+        "noneText": "None of them",
+        "otherText": "Other",
+        "choices": [
+          "MCRN Donnager",
+          "UNN Agatha King",
+          "EMC Frederick Lewis",
+          "Behemoth",
+          "Heart of the Tempest",
+        ]
+      }
+    ]
+  };
+
+  testWidgets("checkbox should properly render extended options",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        localizationsDelegates: const [
+          appLocalizationDelegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        home: Material(
+          child: SurveyWidget(survey: surveyFromJson(extended)!),
+        ),
+      ),
+    );
+
+    await tester.pump();
+    await tester.idle();
+
+    expect(find.byType(CheckboxListTile), findsWidgets);
+    expect(find.text("All of them"), findsOneWidget);
+    expect(find.text("None of them"), findsOneWidget);
+    expect(find.text("Other"), findsOneWidget);
+  });
+
+  testWidgets(
+      "checkbox with other option checked/unchecked should properly render/eliminate the associated text input",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        localizationsDelegates: const [
+          appLocalizationDelegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        home: Material(
+          child: SurveyWidget(survey: surveyFromJson(extended)!),
+        ),
+      ),
+    );
+
+    await tester.pump();
+    await tester.idle();
+    expect(find.byType(TextFormField), findsNothing);
+
+    // check the "Other" option
+    await tester.tap(find.text("Other"));
+    await tester.pump();
+    await tester.idle();
+    expect(find.byType(TextFormField), findsOneWidget);
+
+    // uncheck the "Other" option
+    await tester.tap(find.text("Other"));
+    await tester.pump();
+    await tester.idle();
+    expect(find.byType(TextFormField), findsNothing);
+  });
+
+  testWidgets(
+      "checkbox with any answer beyond pre-defined choices should properly render the associated other text input",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        localizationsDelegates: const [
+          appLocalizationDelegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        home: Material(
+          child: SurveyWidget(
+            survey: surveyFromJson(extended)!,
+            answer: const {
+              "warships": ["UNN Thomas Prince"]
+            },
+          ),
+        ),
+      ),
+    );
+
+    await tester.pump();
+    await tester.idle();
+    expect(find.byType(TextFormField), findsOneWidget);
+    expect(find.text("UNN Thomas Prince"), findsOneWidget);
+
+    // uncheck the "Other" option
+    await tester.tap(find.text("Other"));
+    await tester.pump();
+    await tester.idle();
+    expect(find.byType(TextFormField), findsNothing);
+
+    // re-check the "Other" option
+    await tester.tap(find.text("Other"));
+    await tester.pump();
+    await tester.idle();
+    expect(find.byType(TextFormField), findsOneWidget);
+    expect(find.text("UNN Thomas Prince"), findsNothing);
   });
 }
