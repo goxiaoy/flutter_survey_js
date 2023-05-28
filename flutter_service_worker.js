4ee4379a073cf0b548d4904c63f6593800672186
goxiaoy <goxiaoy@users.noreply.github.com>
2023-05-28 04:19:52 +0000

    deploy: e9d7ff3cee30582f6df68c7b6a2cecc9dc442d23

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,13 +22,13 @@ const RESOURCES = {"flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.json": "dc671cdf68f0d23ba390dfbcb35d9f92",
 "assets/AssetManifest.smcbin": "9705fa64f095ab0a90de9e7dac110a39",
-"index.html": "8dbb601f3e647d9ee38c883be0573983",
-"/": "8dbb601f3e647d9ee38c883be0573983",
+"index.html": "8cfd89c4335de35dde86c2c3df0ff9e3",
+"/": "8cfd89c4335de35dde86c2c3df0ff9e3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "c6dbb76a5a1d7b472dfaedba4bbb8c89",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"main.dart.js": "eebda32afdf53e95badd6641b50be4a5",
+"main.dart.js": "b399a83ce8b582e5b4e3281eefa63952",
 "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b"};
 // The application shell files that are downloaded before a service worker can
 // start.
