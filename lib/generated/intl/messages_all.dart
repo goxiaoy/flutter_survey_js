8ca7c04d44523a0a419f9e891e1c35bcb70f1c00
laogao <gaoyuxiang@gmail.com>
2023-07-11 15:11:15 +0800

    adds locale fr

--- lib/generated/intl/messages_all.dart
+++ lib/generated/intl/messages_all.dart
@@ -17,11 +17,13 @@ import 'package:intl/message_lookup_by_library.dart';
 import 'package:intl/src/intl_helpers.dart';
 
 import 'messages_en.dart' as messages_en;
+import 'messages_fr.dart' as messages_fr;
 import 'messages_zh.dart' as messages_zh;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
   'en': () => new SynchronousFuture(null),
+  'fr': () => new SynchronousFuture(null),
   'zh': () => new SynchronousFuture(null),
 };
 
@@ -29,6 +31,8 @@ MessageLookupByLibrary? _findExact(String localeName) {
   switch (localeName) {
     case 'en':
       return messages_en.messages;
+    case 'fr':
+      return messages_fr.messages;
     case 'zh':
       return messages_zh.messages;
     default:
