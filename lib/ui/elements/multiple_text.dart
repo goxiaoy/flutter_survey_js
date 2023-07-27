d4d72bfbcabb1f1d2a3a9467bd62db5f63509a5a
Goxiaoy <xy1996mail@gmail.com>
2023-07-28 00:06:11 +0800

    multiple_text add InputDecoration

--- lib/ui/elements/multiple_text.dart
+++ lib/ui/elements/multiple_text.dart
@@ -5,33 +5,50 @@ import 'package:flutter_survey_js/ui/reactive/reactive_nested_form.dart';
 import 'package:flutter_survey_js/ui/survey_configuration.dart';
 import 'package:flutter_survey_js_model/flutter_survey_js_model.dart' as s;
 import 'package:reactive_forms/reactive_forms.dart';
-
+import 'package:async/async.dart';
 
 Widget multipleTextBuilder(BuildContext context, s.Elementbase element,
     {ElementConfiguration? configuration}) {
   final e = element as s.Multipletext;
   final texts = (e.items?.toList() ?? []).map(toText).toList();
+
   return ReactiveNestedForm(
       formControlName: e.name,
-      child: ListView.separated(
-        physics: const ClampingScrollPhysics(),
-        shrinkWrap: true,
-        itemCount: texts.length,
-        itemBuilder: (BuildContext context, int index) {
-          final res = SurveyConfiguration.of(context)!
-              .factory
-              .resolve(context, texts[index]);
-          return index == 0
-              ? Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: res,
-                )
-              : res;
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return SurveyConfiguration.of(context)!.separatorBuilder(context);
-        },
-      ).wrapQuestionTitle(context, element, configuration: configuration));
+      child: Builder(builder: (context) {
+        final control = ReactiveForm.of(context) as FormGroup;
+        final effectiveDecoration = const InputDecoration()
+            .applyDefaults(Theme.of(context).inputDecorationTheme);
+        return StreamBuilder(
+          stream:
+              StreamGroup.merge([control.touchChanges, control.statusChanged]),
+          builder: (context, _) {
+            return InputDecorator(
+              decoration: effectiveDecoration.copyWith(
+                  errorText: getErrorTextFromFormControl(context, control)),
+              child: ListView.separated(
+                physics: const ClampingScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: texts.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final res = SurveyConfiguration.of(context)!
+                      .factory
+                      .resolve(context, texts[index]);
+                  return index == 0
+                      ? Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: res,
+                        )
+                      : res;
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return SurveyConfiguration.of(context)!
+                      .separatorBuilder(context);
+                },
+              ),
+            );
+          },
+        );
+      }).wrapQuestionTitle(context, element, configuration: configuration));
 }
 
 AbstractControl multipleTextControlBuilder(
