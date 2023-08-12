c066e6674e91caea621c37d8ce3815eed5a0523a
goxiaoy <goxiaoy@users.noreply.github.com>
2023-08-12 09:22:46 +0000

    deploy: 7d458f82fe3736fee8fdf51d6b788415ea943949

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ff20dd2bf3622cec845b008a6ea796df",
-"/": "ff20dd2bf3622cec845b008a6ea796df",
+"index.html": "187b725edeb8e0e3fa9e77903050c149",
+"/": "187b725edeb8e0e3fa9e77903050c149",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
